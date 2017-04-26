8467d6edfc5032dabc3af5971f2df897f7c3167f
Binay Verma <binay.verma@imaginea.com>
2017-04-26 17:17:04 +0530

    updated production-config

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -15,7 +15,7 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/dist/'),
         filename: '[name]-[hash].min.js',
-        publicPath: '/'
+        publicPath: './'
     },
     plugins: [
         // webpack gives your modules and chunks ids to identify them. Webpack can vary the
