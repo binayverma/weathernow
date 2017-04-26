66df729d02c77a235dadf03be913a62bb7bc766f
Binay Verma <binay.verma@imaginea.com>
2017-04-26 17:19:42 +0530

    using hashHistory

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { AppContainer } from 'react-hot-loader';
 
@@ -11,7 +11,7 @@ import './styles/index.scss';
 import './styles/footer.scss';
 
 const store = configureStore();
-const history = syncHistoryWithStore(browserHistory, store);
+const history = syncHistoryWithStore(hashHistory, store);
 
 render(
     <AppContainer>
